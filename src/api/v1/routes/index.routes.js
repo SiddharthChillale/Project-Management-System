d987608f658c2e1175d1f1dad10d9ea33179a6e7
siddharthchillale <siddharth.chillale@gmail.com>
2024-09-14 23:44:48 +0530

    add routes for all new CRUDs - untested

--- src/api/v1/routes/index.routes.js
+++ src/api/v1/routes/index.routes.js
@@ -3,6 +3,9 @@ import { rootHandler } from "../controllers/index.controllers.js";
 
 import projectRouter from "./project.routes.js";
 import userRouter from "./user.routes.js";
+import eventRouter from "./event.routes.js";
+import departmentRouter from "./department.routes.js";
+import scoreCatRouter from "./scoreCategory.routes.js";
 
 const router = express.Router();
 
@@ -11,5 +14,10 @@ router.get("/", rootHandler);
 // all other routes
 router.use("/api/v1/projects", projectRouter);
 router.use("/api/v1/users", userRouter);
+router.use("/api/v1/events", eventRouter);
+router.use("/api/v1/score-category", scoreCatRouter);
+router.use("/api/v1/departments", departmentRouter);
+// routes for ratings
+// routes for project-associations
 
 export default router;
