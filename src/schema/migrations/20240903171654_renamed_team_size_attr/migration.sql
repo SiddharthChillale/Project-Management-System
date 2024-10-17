83be4f36f195c3015a4867eeb478d68ab7c6cb5f
siddharthchillale <siddharth.chillale@gmail.com>
2024-10-17 08:31:26 +0530

    remove old prisma schema

--- src/schema/migrations/20240903171654_renamed_team_size_attr/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `team_size` on the `Projects` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Projects" DROP COLUMN "team_size",
-ADD COLUMN     "teamSize" INTEGER;
